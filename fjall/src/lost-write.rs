1c332d55bd802fa3672cab1867635434e65685a0
phil <uniphil@gmail.com>
2024-12-18 10:25:55 -0500

    still repros...

--- fjall/src/lost-write.rs
+++ fjall/src/lost-write.rs
@@ -1,9 +1,7 @@
 use std::fs;
 use std::io::{self, BufRead};
-use std::str::FromStr;
 use std::time::Instant;
-use anyhow::{anyhow, Result};
-use fjall::{Config, PersistMode, PartitionCreateOptions};
+use fjall::{Config, PartitionHandle, PersistMode, PartitionCreateOptions};
 use tikv_jemallocator::Jemalloc;
 
 #[global_allocator]
@@ -12,35 +10,17 @@ static GLOBAL: Jemalloc = Jemalloc;
 const DB_PATH: &str = "./likes.fjall";
 const LIKES_PATH: &str = "../likes5M-anon.txt";
 
-const SYNC_STEP: u64 = 100;
+const SYNC_STEP: usize = 100;
 
-#[derive(Debug)]
-enum Action {
-    Create(String),
-    Delete(String),
-}
 
-impl FromStr for Action {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        let Some((action, rest)) = s.split_once(';') else {
-            return Err(anyhow!("line must have at least two fields"))
-        };
-        match action {
-            "c" => {
-                let Some((uri, id)) = rest.split_once(';') else {
-                    return Err(anyhow!("both uri and id for create"))
-                };
-                Ok(Action::Create(format!("{}!{}", uri, id)))
-            }
-            "d" => Ok(Action::Delete(rest.into())),
-            _ => Err(anyhow!("need 'c' or 'd' for entry action type")),
-        }
+fn check_count(part: PartitionHandle, expected: usize) {
+    let counted = part.len().unwrap();
+    if counted != expected {
+        println!("FAIL: {}: found {counted} != {expected}", part.name);
     }
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // ensure a clean start
     fs::remove_dir_all(DB_PATH).or_else(|e| if e.kind() == io::ErrorKind::NotFound { Ok(()) } else { Err(e) })?;
 
@@ -57,22 +37,19 @@ fn main() -> Result<()> {
         let unlikes = keyspace.open_partition("unlikes", PartitionCreateOptions::default())?;
 
         for line in reader.lines() {
-            let action: Action = line?.parse()?;
-            let sync = ((n_likes + n_unlikes) % SYNC_STEP) == (SYNC_STEP - 1);
-
-            if sync {
+            if ((n_likes + n_unlikes) % SYNC_STEP) == (SYNC_STEP - 1) {
                 keyspace.persist(PersistMode::SyncData)?;
             }
-
-            match action {
-                Action::Create(key) => {
-                    likes.insert(&key, "")?;
+            match line?.split_once(';').expect("line must have two fields") {
+                ("c", k) => {
+                    likes.insert(&k, "")?;
                     n_likes += 1;
                 }
-                Action::Delete(key) => {
-                    unlikes.insert(&key, "")?;
+                ("d", k) => {
+                    unlikes.insert(&k, "")?;
                     n_unlikes += 1;
                 }
+                (_, _) => panic!("action must be 'c' or 'd'"),
             }
         }
 
@@ -80,18 +57,8 @@ fn main() -> Result<()> {
         let d = t0.elapsed();
         println!("done in {:.1}s. likes: {}, unlikes: {}", d.as_secs_f32(), n_likes, n_unlikes);
 
-        {
-            let n = likes.iter().count();
-            if n as u64 != n_likes {
-                println!("FAIL: before close: likes {} != {}", n, n_likes);
-            }
-        }
-        {
-            let n = unlikes.iter().count();
-            if n as u64 != n_unlikes {
-                println!("FAIL: before close: unlikes {} != {}", n, n_unlikes);
-            }
-        }
+        check_count(likes, n_likes);
+        check_count(unlikes, n_unlikes);
 
         println!("closing keyspace...");
     }
@@ -102,18 +69,8 @@ fn main() -> Result<()> {
         let likes = keyspace.open_partition("likes", PartitionCreateOptions::default())?;
         let unlikes = keyspace.open_partition("unlikes", PartitionCreateOptions::default())?;
 
-        {
-            let n = likes.iter().count();
-            if n as u64 != n_likes {
-                println!("FAIL: after close: likes {} != {}", n, n_likes);
-            }
-        }
-        {
-            let n = unlikes.iter().count();
-            if n as u64 != n_unlikes {
-                println!("FAIL: after close: unlikes {} != {}", n, n_unlikes);
-            }
-        }
+        check_count(likes, n_likes);
+        check_count(unlikes, n_unlikes);
 
         println!("bye");
     }
