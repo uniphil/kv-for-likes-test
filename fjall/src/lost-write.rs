eb8b1dd6a672556df6d8035eb8219fed4cebcc97
phil <uniphil@gmail.com>
2024-12-18 09:21:44 -0500

    still repros...

--- fjall/src/lost-write.rs
+++ fjall/src/lost-write.rs
@@ -5,13 +5,12 @@ use std::time::{Duration, Instant};
 use anyhow::{anyhow, Result};
 use fjall::{Config, PersistMode, PartitionCreateOptions};
 use tikv_jemallocator::Jemalloc;
-use tinyjson::JsonValue;
 
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
 const DB_PATH: &str = "./likes.fjall";
-const LIKES_PATH: &str = "../likes5-simple-noxxx.jsonl";
+const LIKES_PATH: &str = "../likes5M-anon.txt";
 
 const CHECKIN_STEP: u64 = 10_000;
 const SYNC_STEP: u64 = 100;
@@ -27,44 +26,26 @@ struct Stats {
 
 #[derive(Debug)]
 enum Action {
-    Create(CreateEntry),
-    Delete(DeleteEntry),
-}
-
-#[derive(Debug)]
-struct CreateEntry {
-    did: String,
-    rkey: String,
-    uri: String,
-}
-
-#[derive(Debug)]
-struct DeleteEntry {
-    did: String,
-    rkey: String,
+    Create(String),
+    Delete(String),
 }
 
 impl FromStr for Action {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        let parsed: JsonValue = s.parse()?;
-        let entry = <Vec<_>>::try_from(parsed)?;
-        if entry.len() != 4 {
-            panic!("expected entries of length 4");
-        }
-        let action = String::try_from(entry[0].clone())?;
-        let did = String::try_from(entry[1].clone())?;
-        let rkey = String::try_from(entry[2].clone())?;
-        match action.as_str() {
+        let Some((action, rest)) = s.split_once(';') else {
+            return Err(anyhow!("line must have at least two fields"))
+        };
+        match action {
             "c" => {
-                let uri = String::try_from(entry[3].clone())?;
-                Ok(Action::Create(CreateEntry { did, rkey, uri }))
+                let Some((uri, id)) = rest.split_once(';') else {
+                    return Err(anyhow!("both uri and id for create"))
+                };
+                Ok(Action::Create(format!("{}!{}", uri, id)))
             }
-            "d" => {
-                Ok(Action::Delete(DeleteEntry { did, rkey }))
-            }
-            _ => Err(anyhow!("need 'c' or 'd' for entry action type"))
+            "d" => Ok(Action::Delete(rest.into())),
+            _ => Err(anyhow!("need 'c' or 'd' for entry action type")),
         }
     }
 }
@@ -97,13 +78,11 @@ fn main() -> Result<()> {
             }
 
             match action {
-                Action::Create(entry) => {
-                    let key = format!("{}!{}!{}", entry.uri, entry.did, entry.rkey);
+                Action::Create(key) => {
                     likes.insert(&key, "")?;
                     stats.likes += 1;
                 }
-                Action::Delete(entry) => {
-                    let key = format!("{}!{}", entry.did, entry.rkey);
+                Action::Delete(key) => {
                     unlikes.insert(&key, "")?;
                     stats.unlikes += 1;
                 }
@@ -124,11 +103,17 @@ fn main() -> Result<()> {
         println!("done in {:.1}s. entries: {}, likes: {}, unlikes: {}, subjects: {}",
             d.as_secs_f32(), stats.entries, stats.likes, stats.unlikes, stats.subjects);
 
-        if likes.approximate_len() as u64 != stats.likes {
-            println!("FAIL: before close: likes {} != {}", likes.approximate_len(), stats.likes);
+        {
+            let n = likes.iter().count();
+            if n as u64 != stats.likes {
+                println!("FAIL: before close: likes {} != {}", n, stats.likes);
+            }
         }
-        if unlikes.approximate_len() as u64 != stats.unlikes {
-            println!("FAIL: before close: unlikes {} != {}", unlikes.approximate_len(), stats.unlikes);
+        {
+            let n = unlikes.iter().count();
+            if n as u64 != stats.unlikes {
+                println!("FAIL: before close: unlikes {} != {}", n, stats.unlikes);
+            }
         }
 
         println!("closing keyspace...");
@@ -140,11 +125,17 @@ fn main() -> Result<()> {
         let likes = keyspace.open_partition("likes", PartitionCreateOptions::default())?;
         let unlikes = keyspace.open_partition("unlikes", PartitionCreateOptions::default())?;
 
-        if likes.approximate_len() as u64 != stats.likes {
-            println!("FAIL: after close: likes {} != {}", likes.approximate_len(), stats.likes);
+        {
+            let n = likes.iter().count();
+            if n as u64 != stats.likes {
+                println!("FAIL: after close: likes {} != {}", n, stats.likes);
+            }
         }
-        if unlikes.approximate_len() as u64 != stats.unlikes {
-            println!("FAIL: after close: unlikes {} != {}", unlikes.approximate_len(), stats.unlikes);
+        {
+            let n = unlikes.iter().count();
+            if n as u64 != stats.unlikes {
+                println!("FAIL: after close: unlikes {} != {}", n, stats.unlikes);
+            }
         }
 
         println!("bye");
